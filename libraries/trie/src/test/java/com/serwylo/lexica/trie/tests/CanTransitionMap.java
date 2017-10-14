14112e69a48270d0d6332340dfba7cb6f7bf99f2
Guru <gkathir15@gmail.com>
2017-10-14 10:46:01 +0530

    finale done

--- libraries/trie/src/test/java/com/serwylo/lexica/trie/tests/CanTransitionMap.java
+++ libraries/trie/src/test/java/com/serwylo/lexica/trie/tests/CanTransitionMap.java
@@ -22,7 +22,7 @@ public class CanTransitionMap implements TransitionMap {
 
     CanTransitionMap() {
         this.letters = new String[]{
-                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "qu", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         };
     }
 
